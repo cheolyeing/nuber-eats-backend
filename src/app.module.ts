dfa874c8e1849fa55808855ad61b5375431e223b
Limi <cheolyeing>
2022-01-19 02:52:18 +0900

    3.0 Our First Entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -29,6 +30,7 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @Field(() => Number)
+  @PrimaryColumn()
+  id: number;
+
   @Field(() => String)
+  @Column()
   name: string;
 
   @Field(() => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(() => String)
+  @Column()
   address: string;
 
   @Field(() => String)
+  @Column()
   owenerName: string;
+
+  @Field(() => String)
+  @Column()
+  categoryName: string;
 }
