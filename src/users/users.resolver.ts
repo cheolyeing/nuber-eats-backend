5cc799978d2a6920d18f5b7f1953be4c2b9e73ee
Limi <cheolyeing>
2022-01-21 05:08:01 +0900

    4.6 An Alternative Error

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -15,17 +15,7 @@ export class UsersResolver {
   @Mutation(() => CreateAccountOutput)
   async createAccount(@Args("input") createAccountInput: CreateAccountInput): Promise<CreateAccountOutput> {
     try {
-      const error = await this.usersService.createAccount(createAccountInput);
-      if (error) {
-        return {
-          ok: false,
-          error,
-        };
-      }
-
-      return {
-        ok: true,
-      };
+      return await this.usersService.createAccount(createAccountInput);
     } catch (error) {
       return {
         ok: false,
